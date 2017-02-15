5cba99387de677665f3dae9deeac79eb0a5b55e4
mpedersen15 <mpedersen@mindrocketdesign.com>
2017-02-15 14:14:57 -0700

    Added newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,8 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 	
+	socket.emit('newMessage', {
+		from: 'Matt',
+		text: 'Wanna come over and watch Star Wars?',
+		createdAt: new Date().toString()
+	});
+	
 	socket.on('disconnect', () => {
 		console.log('User disconnected');
+	});
+	
+	socket.on('createMessage', (message) => {
+		console.log('message created: ', message);
 	});
 });
 
