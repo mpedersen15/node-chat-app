192ff2a51e3bcc0ab0a5b1ff9a9e04193399975a
mpedersen15 <mpedersen@mindrocketdesign.com>
2017-02-21 13:55:23 -0700

    Added updateUserList event to server and client

--- server/server.js
+++ server/server.js
@@ -3,15 +3,16 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-var {generateMessage, generateLocationMessage} = require('./utils/message');
-var {isRealString} = require('./utils/validation');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
-
+var users = new Users();
 
 
 app.use(express.static(publicPath));
@@ -23,10 +24,14 @@ io.on('connection', (socket) => {
 	
 	socket.on('join', (params, callback) => {
 		if (!isRealString(params.name) || !isRealString(params.room)){
-			callback('Please enter valid name and room name');
+			return callback('Please enter valid name and room name');
 		}
 		
 		socket.join(params.room);
+		users.removeUser(socket.id);
+		users.addUser(socket.id, params.name, params.room);
+		io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+		
 		// socket.leave(params.room)
 		
 		// io.to(params.room).emit
@@ -42,6 +47,13 @@ io.on('connection', (socket) => {
 	
 	socket.on('disconnect', () => {
 		console.log('User disconnected');
+		var user = users.removeUser(socket.id);
+		
+		if (user){
+			io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+			io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+		}
+		
 	});
 	
 	socket.on('createMessage', (message, callback) => {
